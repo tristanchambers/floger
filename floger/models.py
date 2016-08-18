45f9c1b25e7f0ab7062f07979a0a0fff709847f9
Tristan Chambers <tchambers@smith.edu>
2016-08-18 10:11:10 -0400

    Add models for ingredients and amounts And add to admin panel

--- floger/models.py
+++ floger/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# https://docs.djangoproject.com/en/1.10/topics/db/models/
+# https://docs.djangoproject.com/en/1.10/ref/models/fields/
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class Ingredient_amount(models.Model):
+    AMOUNTS = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    ingredient = models.ForeignKey(Ingredient)
+    amount = models.CharField(max_length=1, choices=AMOUNTS)
