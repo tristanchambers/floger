6fddad0c6290d0c4f96f1ad5e11e1c8602488522
Tristan Chambers <tchambers@smith.edu>
2016-08-18 13:13:16 -0400

    Add Flog model, and attach Ingredient_amount to it via foreign key. Add this to the admin panel bringing Ingredient_amounts in via 'inline'. And add migrations!

--- floger/admin.py
+++ floger/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Ingredient
+from .models import Ingredient_amount
+from .models import Flog
 
 # Register your models here.
+# https://docs.djangoproject.com/en/1.10/ref/contrib/admin/
+
+admin.site.register(Ingredient)
+
+class Ingredient_amountInline(admin.TabularInline):
+    model = Ingredient_amount
+
+class FlogAdmin(admin.ModelAdmin):
+    inlines = [
+        Ingredient_amountInline,
+    ]
+
+admin.site.register(Flog, FlogAdmin)
--- floger/models.py
+++ floger/models.py
@@ -3,9 +3,21 @@ from django.db import models
 # Create your models here.
 # https://docs.djangoproject.com/en/1.10/topics/db/models/
 # https://docs.djangoproject.com/en/1.10/ref/models/fields/
+#
+# Any time you edit this file you have to run:
+# manage.py makemigrations
+# manage.py migrate
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Flog(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return '%s' % (self.date)
 
 class Ingredient_amount(models.Model):
     AMOUNTS = (
@@ -15,3 +27,6 @@ class Ingredient_amount(models.Model):
     )
     ingredient = models.ForeignKey(Ingredient)
     amount = models.CharField(max_length=1, choices=AMOUNTS)
+    flog = models.ForeignKey(Flog)
+    def __str__(self):
+        return '%s %s' % (self.ingredient, self.amount)
